ae62426eec45312d212ac319f76e6dcba674bf85
Cherolyn Lexvold <ahavalexvold@gmail.com>
2020-01-05 14:35:36 -0600

    main.go: added 1+4*2 which is an example of PEMDAS rule.
    To write an example of PEMDAS Rule including a string and the
    solution.
    
    The client's concern needs to be addressed.
    
    This patch illustrates an example of PEMDAS rule including a
    string and the solution.
    
    Signed-off-by: Cherolyn Lexvold <ahavalexvold@gmail.com>

--- main.go
+++ main.go
@@ -7,4 +7,5 @@ import (
 func main(){
 	fmt.Println("I think that life is surprising.")
         fmt.Println("The total for BASIC MATH is:", 1+1)
+	fmt.Println("The total for PEMDAS Rule is:", 1+4*2)
  }
