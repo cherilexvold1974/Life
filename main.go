d6f6b9f39adf22f1718b91b8dbc53e2d7d1c8adb
Cherolyn Lexvold <ahavalexvold@gmail.com>
2020-01-05 21:09:46 -0600

    main.go: various calculations made to explore vim
    The students needs practice using vim in order to become proficient
    in developing.
    
    This patch provides exercises to increase the student's skills.
    
    Signed-off-by: Cherolyn Lexvold <ahavalexvold@gmail.com>

--- main.go
+++ main.go
@@ -8,4 +8,12 @@ func main(){
 	fmt.Println("I think that life is surprising.")
         fmt.Println("The total for BASIC MATH is:", 1+1)
 	fmt.Println("The total for PEMDAS Rule is:", 1+4*2)
+	fmt.Println("The total for Adv.Math is:", 1+(6-2)*4)
+	fmt.Println("The total for Round Divide is:", 52/5.)
+	fmt.Println("The total for Full Quotient Divide is:", 52/5.0)
+	fmt.Println("The total for Tricky Math is:", ((43-22)+15.22-22.33)*4)
+	fmt.Println("The total for Tip Calculation is:", (5.77+2.54+6.33+12.22+44.55)*6/100)
+	fmt.Println("The total for Tax Calculation is:", 22500*33/100)
+	fmt.Println("The total for Funky Math is:", (12+45+67)-(21-5-3))
+	fmt.Println("The total for 23 Times of Earth Diameter is:", 40075017*23)
  }
